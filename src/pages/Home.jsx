f7caa821547dcdf66223b5aa3eafc5e98d8ecdb8
aamirmahboob12 <aamir.mehboob@archiwiz.com>
2024-11-25 01:41:48 -0800

    partner section design

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,11 +1,13 @@
 import React from 'react'
 import MainHero from '../components/HomeComponents/MainHero'
-import Testimonial from '../components/Testimonial'
+import Testimonial from '../components/HomeComponents/Testimonial'
+import ProjComp from '../components/HomeComponents/ProjComp'
 
 const Home = () => {
   return (
     <div className='dark:bg-gray-800 dark:text-gray-200 '>
       <MainHero />
+      <ProjComp />
       <Testimonial />
     </div>
   )
