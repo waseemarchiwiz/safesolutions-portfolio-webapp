2bf2fb359ef913acd30418add46dd12540b852ed
aamirmahboob12 <aamir.mehboob@archiwiz.com>
2024-11-27 02:24:55 -0800

    testimonial section designed

--- src/components/HomeComponents/Testimonial.jsx
+++ src/components/HomeComponents/Testimonial.jsx
@@ -1,280 +1,3 @@
-// import React from 'react'
-
-// const Testimonial = () => {
-//   return (
-     
-      
-//       <section className="bg-[#e4e4e7]">
-//         <div className="mx-auto max-w-screen-2xl px-4 py-12 sm:px-6 lg:px-8 lg:py-16">
-//           <div className="md:flex md:items-end md:justify-between">
-//             <div className="max-w-xl">
-//               <h2 className="text-4xl font-bold tracking-tight text-gray-900 sm:text-5xl">
-//                 Read trusted reviews from our customers
-//               </h2>
-      
-//               <p className="mt-6 max-w-lg leading-relaxed text-gray-700">
-//                 Lorem ipsum dolor sit amet consectetur adipisicing elit. Aspernatur praesentium natus
-//                 sapiente commodi. Aliquid sunt tempore iste repellendus explicabo dignissimos placeat,
-//                 autem harum dolore reprehenderit quis! Quo totam dignissimos earum.
-//               </p>
-//             </div>
-      
-//             <a
-//               href="#"
-//               className="mt-6 inline-flex shrink-0 items-center gap-2 rounded-full border border-rose-600 px-5 py-3 text-rose-600 transition hover:bg-rose-600 hover:text-white md:mt-0"
-//             >
-//               <span className="font-medium"> Read all reviews </span>
-      
-//               <svg
-//                 xmlns="http://www.w3.org/2000/svg"
-//                 className="size-4 rtl:rotate-180"
-//                 fill="none"
-//                 viewBox="0 0 24 24"
-//                 stroke="currentColor"
-//               >
-//                 <path
-//                   strokeLinecap="round"
-//                   strokeLinejoin="round"
-//                   strokeWidth="2"
-//                   d="M14 5l7 7m0 0l-7 7m7-7H3"
-//                 />
-//               </svg>
-//             </a>
-//           </div>
-      
-//           <div className="mt-8 grid grid-cols-1 gap-4 md:grid-cols-3">
-//             <blockquote className="flex h-full flex-col justify-between bg-white p-6 shadow-sm sm:p-8">
-//               <div>
-//                 <div className="flex gap-0.5 text-green-500">
-//                   <svg
-//                     className="size-5"
-//                     fill="currentColor"
-//                     viewBox="0 0 20 20"
-//                     xmlns="http://www.w3.org/2000/svg"
-//                   >
-//                     <path
-//                       d="M9.049 2.927c.3-.921 1.603-.921 1.902 0l1.07 3.292a1 1 0 00.95.69h3.462c.969 0 1.371 1.24.588 1.81l-2.8 2.034a1 1 0 00-.364 1.118l1.07 3.292c.3.921-.755 1.688-1.54 1.118l-2.8-2.034a1 1 0 00-1.175 0l-2.8 2.034c-.784.57-1.838-.197-1.539-1.118l1.07-3.292a1 1 0 00-.364-1.118L2.98 8.72c-.783-.57-.38-1.81.588-1.81h3.461a1 1 0 00.951-.69l1.07-3.292z"
-//                     />
-//                   </svg>
-      
-//                   <svg
-//                     className="size-5"
-//                     fill="currentColor"
-//                     viewBox="0 0 20 20"
-//                     xmlns="http://www.w3.org/2000/svg"
-//                   >
-//                     <path
-//                       d="M9.049 2.927c.3-.921 1.603-.921 1.902 0l1.07 3.292a1 1 0 00.95.69h3.462c.969 0 1.371 1.24.588 1.81l-2.8 2.034a1 1 0 00-.364 1.118l1.07 3.292c.3.921-.755 1.688-1.54 1.118l-2.8-2.034a1 1 0 00-1.175 0l-2.8 2.034c-.784.57-1.838-.197-1.539-1.118l1.07-3.292a1 1 0 00-.364-1.118L2.98 8.72c-.783-.57-.38-1.81.588-1.81h3.461a1 1 0 00.951-.69l1.07-3.292z"
-//                     />
-//                   </svg>
-      
-//                   <svg
-//                     className="size-5"
-//                     fill="currentColor"
-//                     viewBox="0 0 20 20"
-//                     xmlns="http://www.w3.org/2000/svg"
-//                   >
-//                     <path
-//                       d="M9.049 2.927c.3-.921 1.603-.921 1.902 0l1.07 3.292a1 1 0 00.95.69h3.462c.969 0 1.371 1.24.588 1.81l-2.8 2.034a1 1 0 00-.364 1.118l1.07 3.292c.3.921-.755 1.688-1.54 1.118l-2.8-2.034a1 1 0 00-1.175 0l-2.8 2.034c-.784.57-1.838-.197-1.539-1.118l1.07-3.292a1 1 0 00-.364-1.118L2.98 8.72c-.783-.57-.38-1.81.588-1.81h3.461a1 1 0 00.951-.69l1.07-3.292z"
-//                     />
-//                   </svg>
-      
-//                   <svg
-//                     className="size-5"
-//                     fill="currentColor"
-//                     viewBox="0 0 20 20"
-//                     xmlns="http://www.w3.org/2000/svg"
-//                   >
-//                     <path
-//                       d="M9.049 2.927c.3-.921 1.603-.921 1.902 0l1.07 3.292a1 1 0 00.95.69h3.462c.969 0 1.371 1.24.588 1.81l-2.8 2.034a1 1 0 00-.364 1.118l1.07 3.292c.3.921-.755 1.688-1.54 1.118l-2.8-2.034a1 1 0 00-1.175 0l-2.8 2.034c-.784.57-1.838-.197-1.539-1.118l1.07-3.292a1 1 0 00-.364-1.118L2.98 8.72c-.783-.57-.38-1.81.588-1.81h3.461a1 1 0 00.951-.69l1.07-3.292z"
-//                     />
-//                   </svg>
-      
-//                   <svg
-//                     className="size-5"
-//                     fill="currentColor"
-//                     viewBox="0 0 20 20"
-//                     xmlns="http://www.w3.org/2000/svg"
-//                   >
-//                     <path
-//                       d="M9.049 2.927c.3-.921 1.603-.921 1.902 0l1.07 3.292a1 1 0 00.95.69h3.462c.969 0 1.371 1.24.588 1.81l-2.8 2.034a1 1 0 00-.364 1.118l1.07 3.292c.3.921-.755 1.688-1.54 1.118l-2.8-2.034a1 1 0 00-1.175 0l-2.8 2.034c-.784.57-1.838-.197-1.539-1.118l1.07-3.292a1 1 0 00-.364-1.118L2.98 8.72c-.783-.57-.38-1.81.588-1.81h3.461a1 1 0 00.951-.69l1.07-3.292z"
-//                     />
-//                   </svg>
-//                 </div>
-      
-//                 <div className="mt-4">
-//                   <p className="text-2xl font-bold text-rose-600 sm:text-3xl">Stayin' Alive</p>
-      
-//                   <p className="mt-4 leading-relaxed text-gray-700">
-//                     No, Rose, they are not breathing. And they have no arms or legs … Where are they? You
-//                     know what? If we come across somebody with no arms or legs, do we bother resuscitating
-//                     them? I mean, what quality of life do we have there?
-//                   </p>
-//                 </div>
-//               </div>
-      
-//               <footer className="mt-4 text-sm font-medium text-gray-700 sm:mt-6">
-//                 &mdash; Michael Scott
-//               </footer>
-//             </blockquote>
-      
-//             <blockquote className="flex h-full flex-col justify-between bg-white p-6 shadow-sm sm:p-8">
-//               <div>
-//                 <div className="flex gap-0.5 text-green-500">
-//                   <svg
-//                     className="size-5"
-//                     fill="currentColor"
-//                     viewBox="0 0 20 20"
-//                     xmlns="http://www.w3.org/2000/svg"
-//                   >
-//                     <path
-//                       d="M9.049 2.927c.3-.921 1.603-.921 1.902 0l1.07 3.292a1 1 0 00.95.69h3.462c.969 0 1.371 1.24.588 1.81l-2.8 2.034a1 1 0 00-.364 1.118l1.07 3.292c.3.921-.755 1.688-1.54 1.118l-2.8-2.034a1 1 0 00-1.175 0l-2.8 2.034c-.784.57-1.838-.197-1.539-1.118l1.07-3.292a1 1 0 00-.364-1.118L2.98 8.72c-.783-.57-.38-1.81.588-1.81h3.461a1 1 0 00.951-.69l1.07-3.292z"
-//                     />
-//                   </svg>
-      
-//                   <svg
-//                     className="size-5"
-//                     fill="currentColor"
-//                     viewBox="0 0 20 20"
-//                     xmlns="http://www.w3.org/2000/svg"
-//                   >
-//                     <path
-//                       d="M9.049 2.927c.3-.921 1.603-.921 1.902 0l1.07 3.292a1 1 0 00.95.69h3.462c.969 0 1.371 1.24.588 1.81l-2.8 2.034a1 1 0 00-.364 1.118l1.07 3.292c.3.921-.755 1.688-1.54 1.118l-2.8-2.034a1 1 0 00-1.175 0l-2.8 2.034c-.784.57-1.838-.197-1.539-1.118l1.07-3.292a1 1 0 00-.364-1.118L2.98 8.72c-.783-.57-.38-1.81.588-1.81h3.461a1 1 0 00.951-.69l1.07-3.292z"
-//                     />
-//                   </svg>
-      
-//                   <svg
-//                     className="size-5"
-//                     fill="currentColor"
-//                     viewBox="0 0 20 20"
-//                     xmlns="http://www.w3.org/2000/svg"
-//                   >
-//                     <path
-//                       d="M9.049 2.927c.3-.921 1.603-.921 1.902 0l1.07 3.292a1 1 0 00.95.69h3.462c.969 0 1.371 1.24.588 1.81l-2.8 2.034a1 1 0 00-.364 1.118l1.07 3.292c.3.921-.755 1.688-1.54 1.118l-2.8-2.034a1 1 0 00-1.175 0l-2.8 2.034c-.784.57-1.838-.197-1.539-1.118l1.07-3.292a1 1 0 00-.364-1.118L2.98 8.72c-.783-.57-.38-1.81.588-1.81h3.461a1 1 0 00.951-.69l1.07-3.292z"
-//                     />
-//                   </svg>
-      
-//                   <svg
-//                     className="size-5"
-//                     fill="currentColor"
-//                     viewBox="0 0 20 20"
-//                     xmlns="http://www.w3.org/2000/svg"
-//                   >
-//                     <path
-//                       d="M9.049 2.927c.3-.921 1.603-.921 1.902 0l1.07 3.292a1 1 0 00.95.69h3.462c.969 0 1.371 1.24.588 1.81l-2.8 2.034a1 1 0 00-.364 1.118l1.07 3.292c.3.921-.755 1.688-1.54 1.118l-2.8-2.034a1 1 0 00-1.175 0l-2.8 2.034c-.784.57-1.838-.197-1.539-1.118l1.07-3.292a1 1 0 00-.364-1.118L2.98 8.72c-.783-.57-.38-1.81.588-1.81h3.461a1 1 0 00.951-.69l1.07-3.292z"
-//                     />
-//                   </svg>
-      
-//                   <svg
-//                     className="size-5"
-//                     fill="currentColor"
-//                     viewBox="0 0 20 20"
-//                     xmlns="http://www.w3.org/2000/svg"
-//                   >
-//                     <path
-//                       d="M9.049 2.927c.3-.921 1.603-.921 1.902 0l1.07 3.292a1 1 0 00.95.69h3.462c.969 0 1.371 1.24.588 1.81l-2.8 2.034a1 1 0 00-.364 1.118l1.07 3.292c.3.921-.755 1.688-1.54 1.118l-2.8-2.034a1 1 0 00-1.175 0l-2.8 2.034c-.784.57-1.838-.197-1.539-1.118l1.07-3.292a1 1 0 00-.364-1.118L2.98 8.72c-.783-.57-.38-1.81.588-1.81h3.461a1 1 0 00.951-.69l1.07-3.292z"
-//                     />
-//                   </svg>
-//                 </div>
-      
-//                 <div className="mt-4">
-//                   <p className="text-2xl font-bold text-rose-600 sm:text-3xl">Stayin' Alive</p>
-      
-//                   <p className="mt-4 leading-relaxed text-gray-700">
-//                     No, Rose, they are not breathing. And they have no arms or legs … Where are they? You
-//                     know what? If we come across somebody with no arms or legs, do we bother resuscitating
-//                     them? I mean, what quality of life do we have there?
-//                   </p>
-//                 </div>
-//               </div>
-      
-//               <footer className="mt-4 text-sm font-medium text-gray-700 sm:mt-6">
-//                 &mdash; Michael Scott
-//               </footer>
-//             </blockquote>
-      
-//             <blockquote className="flex h-full flex-col justify-between bg-white p-6 shadow-sm sm:p-8">
-//               <div>
-//                 <div className="flex gap-0.5 text-green-500">
-//                   <svg
-//                     className="size-5"
-//                     fill="currentColor"
-//                     viewBox="0 0 20 20"
-//                     xmlns="http://www.w3.org/2000/svg"
-//                   >
-//                     <path
-//                       d="M9.049 2.927c.3-.921 1.603-.921 1.902 0l1.07 3.292a1 1 0 00.95.69h3.462c.969 0 1.371 1.24.588 1.81l-2.8 2.034a1 1 0 00-.364 1.118l1.07 3.292c.3.921-.755 1.688-1.54 1.118l-2.8-2.034a1 1 0 00-1.175 0l-2.8 2.034c-.784.57-1.838-.197-1.539-1.118l1.07-3.292a1 1 0 00-.364-1.118L2.98 8.72c-.783-.57-.38-1.81.588-1.81h3.461a1 1 0 00.951-.69l1.07-3.292z"
-//                     />
-//                   </svg>
-      
-//                   <svg
-//                     className="size-5"
-//                     fill="currentColor"
-//                     viewBox="0 0 20 20"
-//                     xmlns="http://www.w3.org/2000/svg"
-//                   >
-//                     <path
-//                       d="M9.049 2.927c.3-.921 1.603-.921 1.902 0l1.07 3.292a1 1 0 00.95.69h3.462c.969 0 1.371 1.24.588 1.81l-2.8 2.034a1 1 0 00-.364 1.118l1.07 3.292c.3.921-.755 1.688-1.54 1.118l-2.8-2.034a1 1 0 00-1.175 0l-2.8 2.034c-.784.57-1.838-.197-1.539-1.118l1.07-3.292a1 1 0 00-.364-1.118L2.98 8.72c-.783-.57-.38-1.81.588-1.81h3.461a1 1 0 00.951-.69l1.07-3.292z"
-//                     />
-//                   </svg>
-      
-//                   <svg
-//                     className="size-5"
-//                     fill="currentColor"
-//                     viewBox="0 0 20 20"
-//                     xmlns="http://www.w3.org/2000/svg"
-//                   >
-//                     <path
-//                       d="M9.049 2.927c.3-.921 1.603-.921 1.902 0l1.07 3.292a1 1 0 00.95.69h3.462c.969 0 1.371 1.24.588 1.81l-2.8 2.034a1 1 0 00-.364 1.118l1.07 3.292c.3.921-.755 1.688-1.54 1.118l-2.8-2.034a1 1 0 00-1.175 0l-2.8 2.034c-.784.57-1.838-.197-1.539-1.118l1.07-3.292a1 1 0 00-.364-1.118L2.98 8.72c-.783-.57-.38-1.81.588-1.81h3.461a1 1 0 00.951-.69l1.07-3.292z"
-//                     />
-//                   </svg>
-      
-//                   <svg
-//                     className="size-5"
-//                     fill="currentColor"
-//                     viewBox="0 0 20 20"
-//                     xmlns="http://www.w3.org/2000/svg"
-//                   >
-//                     <path
-//                       d="M9.049 2.927c.3-.921 1.603-.921 1.902 0l1.07 3.292a1 1 0 00.95.69h3.462c.969 0 1.371 1.24.588 1.81l-2.8 2.034a1 1 0 00-.364 1.118l1.07 3.292c.3.921-.755 1.688-1.54 1.118l-2.8-2.034a1 1 0 00-1.175 0l-2.8 2.034c-.784.57-1.838-.197-1.539-1.118l1.07-3.292a1 1 0 00-.364-1.118L2.98 8.72c-.783-.57-.38-1.81.588-1.81h3.461a1 1 0 00.951-.69l1.07-3.292z"
-//                     />
-//                   </svg>
-      
-//                   <svg
-//                     className="size-5"
-//                     fill="currentColor"
-//                     viewBox="0 0 20 20"
-//                     xmlns="http://www.w3.org/2000/svg"
-//                   >
-//                     <path
-//                       d="M9.049 2.927c.3-.921 1.603-.921 1.902 0l1.07 3.292a1 1 0 00.95.69h3.462c.969 0 1.371 1.24.588 1.81l-2.8 2.034a1 1 0 00-.364 1.118l1.07 3.292c.3.921-.755 1.688-1.54 1.118l-2.8-2.034a1 1 0 00-1.175 0l-2.8 2.034c-.784.57-1.838-.197-1.539-1.118l1.07-3.292a1 1 0 00-.364-1.118L2.98 8.72c-.783-.57-.38-1.81.588-1.81h3.461a1 1 0 00.951-.69l1.07-3.292z"
-//                     />
-//                   </svg>
-//                 </div>
-      
-//                 <div className="mt-4">
-//                   <p className="text-2xl font-bold text-rose-600 sm:text-3xl">Stayin' Alive</p>
-      
-//                   <p className="mt-4 leading-relaxed text-gray-700">
-//                     No, Rose, they are not breathing. And they have no arms or legs … Where are they? You
-//                     know what? If we come across somebody with no arms or legs, do we bother resuscitating
-//                     them? I mean, what quality of life do we have there?
-//                   </p>
-//                 </div>
-//               </div>
-      
-//               <footer className="mt-4 text-sm font-medium text-gray-700 sm:mt-6">
-//                 &mdash; Michael Scott
-//               </footer>
-//             </blockquote>
-//           </div>
-//         </div>
-//       </section> 
-//   )
-// }
-
-// export default Testimonial
-
 import React from "react";
 import { Swiper, SwiperSlide } from "swiper/react";
 import { Navigation, Pagination, Autoplay } from "swiper/modules";
@@ -310,11 +33,21 @@ const Testimonial = () => {
   return (
     <section className="bg-[#e4e4e7] text-black  dark:bg-black py-20">
       <div className="container mx-auto px-6">
-        <h2 className="text-4xl font-bold text-center dark:text-white mb-12 mb-8 ">
+        {/* <h2 className="text-3xl font-bold text-center text-neutral-800 dark:text-white mt-10 ">
           First-Hand Opinions of Clients on Their Partnership Experience
         </h2>
         <p className="text-center text-black dark:text-white mb-12">
-          We are a software and mobile application development company that ensures its expertise extends to offer a seamlessly productive and growth-oriented partnership to its clients.
+          We are a software and mobile application development company that
+          ensures its expertise extends to offer a seamlessly productive and
+          growth-oriented partnership to its clients.
+        </p> */}
+        <h1 className="text-3xl font-bold text-center text-neutral-800 dark:text-white mt-10">
+          First-Hand Opinions of Clients on Their Partnership Experience
+        </h1>
+        <p className=" font-light leading-[33px] text-black dark:text-white mt-4  text-center  w-auto   ">
+          We are a software and mobile application development company that
+          ensures its expertise extends to offer a seamlessly productive and
+          growth-oriented partnership to its clients.
         </p>
 
         <Swiper
@@ -329,8 +62,10 @@ const Testimonial = () => {
         >
           {testimonials.map((testimonial, index) => (
             <SwiperSlide key={index}>
-              <div className=" flex justify-center   flex-col text-black  dark:text-white p-8 rounded-xl shadow-lg  dark:shadow-white  mx-auto h-96 max-w-xl">
-                <p className="text-black dark:text-white italic mb-4">"{testimonial.text}"</p>
+              <div className=" flex justify-center   flex-col text-black  dark:text-white p-8 rounded-xl shadow-lg  dark:shadow-[#1f2937]  mx-auto h-96 max-w-xl">
+                <p className="text-black dark:text-white italic mb-4">
+                  "{testimonial.text}"
+                </p>
                 <div className="flex items-center mt-6">
                   <img
                     src={testimonial.image}
@@ -339,8 +74,12 @@ const Testimonial = () => {
                   />
                   <div>
                     <h3 className="font-bold">{testimonial.name}</h3>
-                    <p className="text-sm text-gray-400">{testimonial.position}</p>
-                    <p className="text-sm text-green-500">{testimonial.company}</p>
+                    <p className="text-sm text-gray-400">
+                      {testimonial.position}
+                    </p>
+                    <p className="text-sm text-green-500">
+                      {testimonial.company}
+                    </p>
                   </div>
                 </div>
               </div>
@@ -353,4 +92,3 @@ const Testimonial = () => {
 };
 
 export default Testimonial;
-
