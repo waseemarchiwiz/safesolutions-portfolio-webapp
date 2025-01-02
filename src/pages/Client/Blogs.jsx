57d99efa406da7c9545481f1e3f13e3c844c1457
aamirmahboob12 <aamir.mehboob@archiwiz.com>
2025-01-02 03:58:59 -0800

    Dynamic routing for blog details

--- src/pages/Client/Blogs.jsx
+++ src/pages/Client/Blogs.jsx
@@ -3,72 +3,56 @@ import { motion } from "framer-motion";
 import CustomButton from "@/globals/CustomButton";
 import { useNavigate } from "react-router-dom";
 import ScrollToTop from "@/globals/ScrollToTop";
-
+import { blogPosts } from "../../lib/Blog";
 const BlogPage = () => {
   const navigate = useNavigate();
-  const blogPosts = [
-    {
-      id: 1,
-      title: "Best Treehouse Plans",
-      date: "2024-12-01",
+  // const blogPosts = [
+  //   {
+  //     id: 1,
+  //     title: "Best Treehouse Plans",
+  //     date: "2024-12-01",
 
-      category: "Architecture",
-      description:
-        "Integrate and build sustainable homes with our new plans. Explore modern techniques for eco-friendly and innovative structures.",
-      image:
-        "https://cdn.pixabay.com/photo/2021/08/04/13/06/software-developer-6521720_640.jpg",
-    },
-    {
-      id: 2,
-      title: "The Future of Buildings",
-      date: "2024-12-05",
+  //     category: "Architecture",
+  //     description:
+  //       "Integrate and build sustainable homes with our new plans. Explore modern techniques for eco-friendly and innovative structures.",
+  //     image:
+  //       "https://cdn.pixabay.com/photo/2021/08/04/13/06/software-developer-6521720_640.jpg",
+  //   },
+  //   {
+  //     id: 2,
+  //     title: "The Future of Buildings",
+  //     date: "2024-12-05",
 
-      category: "Construction",
-      description:
-        "A look into the innovations shaping the future of construction and sustainable development for buildings.",
-      image:
-        "https://cdn.pixabay.com/photo/2014/12/15/14/05/home-office-569153_640.jpg",
-    },
-    {
-      id: 3,
-      title: "Construction Safety Tips",
-      date: "2024-12-03",
+  //     category: "Construction",
+  //     description:
+  //       "A look into the innovations shaping the future of construction and sustainable development for buildings.",
+  //     image:
+  //       "https://cdn.pixabay.com/photo/2014/12/15/14/05/home-office-569153_640.jpg",
+  //   },
+  //   {
+  //     id: 3,
+  //     title: "Construction Safety Tips",
+  //     date: "2024-12-03",
 
-      category: "Safety",
-      description:
-        "Learn essential safety tips to ensure a secure working environment for construction projects.",
-      image:
-        "https://cdn.pixabay.com/photo/2016/11/23/14/49/controls-1853330_640.jpg",
-    },
-    {
-      id: 4,
-      title: "Modern Office Designs",
-      date: "2024-12-02",
+  //     category: "Safety",
+  //     description:
+  //       "Learn essential safety tips to ensure a secure working environment for construction projects.",
+  //     image:
+  //       "https://cdn.pixabay.com/photo/2016/11/23/14/49/controls-1853330_640.jpg",
+  //   },
+  //   {
+  //     id: 4,
+  //     title: "Modern Office Designs",
+  //     date: "2024-12-02",
 
-      category: "Architecture",
-      description:
-        "Explore modern office designs that boost productivity and promote sustainability in the workplace.",
-      image: "https://via.placeholder.com/600x400",
-    },
-    {
-      id: 5,
-      title: "Green Energy Construction",
-      date: "2024-12-07",
+  //     category: "Architecture",
+  //     description:
+  //       "Explore modern office designs that boost productivity and promote sustainability in the workplace.",
+  //     image: "https://via.placeholder.com/600x400",
+  //   },
+  // ];
 
-      category: "EcoBuilding",
-      description:
-        "Harness green energy for sustainable construction projects. Learn about solar and wind energy applications.",
-      image: "https://via.placeholder.com/600x400",
-    },
-  ];
-
-  const categories = [
-    "All",
-    "Architecture",
-    "Construction",
-    "Safety",
-    "EcoBuilding",
-  ];
+  const categories = ["All", "Architecture", "Construction", "Safety"];
 
   // State for pagination and category filtering
   const [currentPage, setCurrentPage] = useState(1);
@@ -133,24 +117,6 @@ const BlogPage = () => {
       <div className="container dark:bg-[#18181b]  mx-auto w-[75%] grid grid-cols-1 lg:grid-cols-3 gap-16 mt-20">
         {/* Main Content */}
         <div className="lg:col-span-2 dark:bg-[#18181b] ">
-          {/* Category Filter */}
-          {/* <div className="mb-6">
-            <label className="font-semibold text-gray-700 mr-2">
-              Category:
-            </label>
-            <select
-              value={selectedCategory}
-              onChange={(e) => handleCategoryChange(e.target.value)}
-              className="border w-52 rounded-md px-4 py-2 focus:outline-none focus:ring-2 focus:ring-blue-400"
-            >
-              {categories.map((category, index) => (
-                <option key={index} value={category}>
-                  {category}
-                </option>
-              ))}
-            </select>
-          </div> */}
-
           {/* Blog Posts */}
           {selectedPosts.map((post, index) => (
             <div
@@ -206,7 +172,7 @@ const BlogPage = () => {
             marginTop: "5px",
           }}
         >
-          <div class="flex lg:ml-auto max-lg:w-full">
+          {/* <div class="flex lg:ml-auto max-lg:w-full">
             <div class="flex xl:w-80 max-xl:w-full bg-gray-100 dark:bg-[#18181b] dark:text-white px-4 py-3 rounded outline outline-transparent border focus-within:border-blue-600 focus-within:bg-transparent transition-all">
               <input
                 type="text"
@@ -222,10 +188,10 @@ const BlogPage = () => {
                 <path d="m190.707 180.101-47.078-47.077c11.702-14.072 18.752-32.142 18.752-51.831C162.381 36.423 125.959 0 81.191 0 36.422 0 0 36.423 0 81.193c0 44.767 36.422 81.187 81.191 81.187 19.688 0 37.759-7.049 51.831-18.751l47.079 47.078a7.474 7.474 0 0 0 5.303 2.197 7.498 7.498 0 0 0 5.303-12.803zM15 81.193C15 44.694 44.693 15 81.191 15c36.497 0 66.189 29.694 66.189 66.193 0 36.496-29.692 66.187-66.189 66.187C44.693 147.38 15 117.689 15 81.193z"></path>
               </svg>
             </div>
-          </div>
+          </div> */}
 
           {/* Categories */}
-          <div className="bg-white dark:text-white dark:bg-[#18181b]  w-[70%]  mb-6 mt-10 ml-1">
+          <div className="bg-white dark:text-white dark:bg-[#18181b]  w-[70%]  mb-6   ml-1">
             <h3 className="text-lg hover:text-[#4f46e5] dark:text-white tracking-wide  mb-4">
               Categories
             </h3>
@@ -241,9 +207,9 @@ const BlogPage = () => {
                   >
                     {cat}
                   </li>
-                  <h3 className="  text-gray-500 dark:text-white hover:text-[#216eb5]  mb-4">
-                    (2)
-                  </h3>
+                  {/* <h3 className="  text-gray-500 dark:text-white hover:text-[#216eb5]  mb-4">
+                   
+                  </h3> */}
                 </div>
               ))}
             </ul>
@@ -277,10 +243,10 @@ const BlogPage = () => {
 
           {/* Tags */}
           <div className="bg-white   dark:bg-[#18181b]   p-4 rounded-md">
-            <h3 className="text-lg font-semibold dark:text-white mb-4">
+            {/* <h3 className="text-lg font-semibold dark:text-white mb-4">
               Tags Cloud
-            </h3>
-            <div className="flex flex-wrap gap-2 ">
+            </h3> */}
+            {/* <div className="flex flex-wrap gap-2 ">
               {["Buildings", "Architectural", "Interior", "Construction"].map(
                 (tag, idx) => (
                   <span
@@ -291,7 +257,7 @@ const BlogPage = () => {
                   </span>
                 )
               )}
-            </div>
+            </div> */}
           </div>
         </aside>
       </div>
