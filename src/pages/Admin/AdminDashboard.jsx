4f7ef2b448994e0313be1f018b0c4290afabc829
aamirmahboob12 <aamir.mehboob@archiwiz.com>
2025-01-17 03:11:07 -0800

    Admin Dashboard KPi Api integrated

--- src/pages/Admin/AdminDashboard.jsx
+++ src/pages/Admin/AdminDashboard.jsx
@@ -1,41 +1,59 @@
-// import KPICard from "@/components/AdminComponents/KpiCard";
-// import DashboardChart from "../../components/AdminComponents/DashboardLineCharts";
-// import React from "react";
-
-// const AdminDashboard = () => {
-//   return (
-//     <div className="p-10">
-//       <h1 className="text-[30px] ml-5">Admin Dashboard</h1>
-//       <div className="flex flex-wrap gap-4">
-//         <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 gap-6 p-6   ">
-//           <KPICard title="Blogs" value="42" change="5.2%"  />
-//           <KPICard title="Testimonials" value="28" change="3.7%" />
-//           <KPICard title="Services" value="6" change="2.1%" />
-//           <KPICard title="Careers" value="12" change="4.5%" />
-//         </div>
-//       </div>
-//       <div className="ml-6">
-//         <DashboardChart />
-//       </div>
-//     </div>
-//   );
-// };
-
-// export default AdminDashboard;
-
 import KPICard from "@/components/AdminComponents/KpiCard";
 import DashboardChart from "../../components/AdminComponents/DashboardLineCharts";
-import React from "react";
+import React, { useEffect, useState } from "react";
+import axios from "axios";
+import apiInstance from "../../../api-config";
 
 const AdminDashboard = () => {
+  const [kpiData, setKpiData] = useState();
+  const userToken = localStorage.getItem("apiusertoken");
+  const fetchKpi = async () => {
+    try {
+      const response = await apiInstance.get("/dashboard", {
+        headers: {
+          user_access_token: userToken,
+        },
+      });
+      if (response?.data?.success) {
+        setKpiData(response?.data?.counts);
+      } else {
+        toast.error("Failed to fetch KPI data");
+      }
+    } catch (error) {}
+  };
+  console.log(kpiData, "kpi data");
+  useEffect(() => {
+    fetchKpi();
+  }, []);
+
   return (
     <div className="   p-10 ">
       <h1 className="text-[30px] text-black ml-5  ">Admin Dashboard</h1>
       <div className=" p-5  mt-3 grid grid-cols-1 sm:grid-cols-2 md:grid-cols-2 lg:grid-cols-4 xl:grid-cols-4 gap-5 sm:gap-5 md:gap-2 lg:gap-8 xl:gap-10">
-        <KPICard title="Blogs" value="42" change="5.2%" />
-        <KPICard title="Blogs" value="42" change="5.2%" />
-        <KPICard title="Blogs" value="42" change="5.2%" />
-        <KPICard title="Blogs" value="42" change="5.2%" />
+        <KPICard
+          title="Blogs"
+          value={kpiData?.getTotalBlogCount?.totalBlogs}
+          change="5.2%"
+        />
+        <KPICard
+          title="Careers"
+          value={kpiData?.getTotalCareerCount?.totalCareer}
+          change="5.2%"
+        />
+        <KPICard
+          title="Testimonials"
+          value={
+            kpiData.getTotalTestimonialCount.length > 0
+              ? kpiData?.getTotalTestimonialCount?.totalTestimonial
+              : "3"
+          }
+          change="5.2%"
+        />
+        <KPICard
+          title="Teams"
+          value={kpiData?.getTotalTeamCount?.totalTeam || 4}
+          change="5.2%"
+        />
       </div>
 
       <div className="p-5">
