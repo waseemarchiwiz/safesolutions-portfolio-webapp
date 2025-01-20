0b76a9a1f7cf95f75a49057eb5ac680e4b3aad02
aamirmahboob12 <aamir.mehboob@archiwiz.com>
2025-01-19 23:43:11 -0800

    Admin Dashboard api fixing

--- src/pages/Admin/AdminDashboard.jsx
+++ src/pages/Admin/AdminDashboard.jsx
@@ -14,6 +14,7 @@ const AdminDashboard = () => {
           user_access_token: userToken,
         },
       });
+      console.log(response, "kpi response: ");
       if (response?.data?.success) {
         setKpiData(response?.data?.counts);
       } else {
@@ -27,31 +28,32 @@ const AdminDashboard = () => {
   }, []);
 
   return (
-    <div className="   p-10 ">
+    <div className="p-10">
       <h1 className="text-[30px] text-black ml-5  ">Admin Dashboard</h1>
       <div className=" p-5  mt-3 grid grid-cols-1 sm:grid-cols-2 md:grid-cols-2 lg:grid-cols-4 xl:grid-cols-4 gap-5 sm:gap-5 md:gap-2 lg:gap-8 xl:gap-10">
         <KPICard
           title="Blogs"
-          value={kpiData?.getTotalBlogCount?.totalBlogs}
+          value={kpiData?.getTotalBlogCount?.totalBlogs || 5}
+          // value="2"
           change="5.2%"
         />
         <KPICard
           title="Careers"
-          value={kpiData?.getTotalCareerCount?.totalCareer}
+          value={kpiData?.getTotalCareerCount?.totalCareer || 2}
+          // value="1"
           change="5.2%"
         />
+
         <KPICard
           title="Testimonials"
-          value={
-            kpiData.getTotalTestimonialCount.length > 0
-              ? kpiData?.getTotalTestimonialCount?.totalTestimonial
-              : "3"
-          }
+          value={kpiData?.getTotalTestimonialCount?.totalTestimonial || 3}
+          // value="3"
           change="5.2%"
         />
         <KPICard
           title="Teams"
           value={kpiData?.getTotalTeamCount?.totalTeam || 4}
+          // value="3"
           change="5.2%"
         />
       </div>
