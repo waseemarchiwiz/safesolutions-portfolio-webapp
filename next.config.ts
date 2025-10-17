9db522183a5e3d24a726b72cfce593cfb07fdc65
wassii-khan <150877571+wassii-khan-git@users.noreply.github.com>
2025-10-17 17:51:27 +0500

    feat: updated

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,7 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   // maximum 10 mb files
+  output: "standalone",
   experimental: {
     serverActions: {
       bodySizeLimit: "10mb",
