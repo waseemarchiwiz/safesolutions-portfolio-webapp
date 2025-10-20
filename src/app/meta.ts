98e4f7c573af1bd2e1515abd1a3ab416a2041213
wassii-khan <waseem@archiwiz.com>
2025-10-20 18:01:39 +0500

    feat: change favicon add meta data

---
+++ src/app/meta.ts
@@ -0,0 +1,4 @@
+export const title =
+  "Safe Solutions | Expert IT Consulting & Software Development";
+export const description =
+  "Safe Solutions provides cutting-edge IT consulting, custom software development, and technology solutions to help businesses thrive in the digital age";
