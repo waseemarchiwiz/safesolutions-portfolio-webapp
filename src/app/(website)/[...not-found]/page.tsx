d497dc26c2ade68e2506ee10f0bb6f9d7e250ef4
wassii-khan <shreddedunion114@gmail.com>
2026-02-04 14:09:17 +0500

    feat: add NotFound component and page for handling 404 errors

---
+++ src/app/(website)/[...not-found]/page.tsx
@@ -0,0 +1,5 @@
+import NotFound from "@/components/common/not-found";
+
+export default function NotFoundPage() {
+  return <NotFound />;
+}
---
+++ src/components/common/not-found.tsx
@@ -0,0 +1,15 @@
+import PageHeroSection from "@/app/(website)/(common)/hero-section";
+/**
+ * We've used catch all route to handle 404 error page
+ **/
+
+export default function NotFound() {
+  return (
+    <PageHeroSection
+      tag="404"
+      title="Page not found"
+      description="Sorry, we couldn’t find the page you’re looking for. Check the URL or return home to start over."
+      isNotFound={true}
+    />
+  );
+}
