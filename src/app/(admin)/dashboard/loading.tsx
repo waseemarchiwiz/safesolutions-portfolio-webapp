7219ad535d8322f049d09dcdeb8bab5dd8aebd12
wassii-khan <shreddedunion114@gmail.com>
2026-02-06 12:31:55 +0500

    fix: set maximum width for loading container in Loading component

--- src/app/(admin)/dashboard/loading.tsx
+++ src/app/(admin)/dashboard/loading.tsx
@@ -2,7 +2,7 @@ import { Card } from "@/components/ui/card";
 
 export default function Loading() {
   return (
-    <div className="container mx-auto py-5 space-y-8">
+    <div className="container max-w-7xl mx-auto py-5 space-y-8">
       <div className="animate-pulse grid grid-cols-1 md:grid-cols-4 gap-4">
         {Array.from({ length: 8 }).map((_, index) => (
           <Card key={index} className="bg-zinc-50 h-52"></Card>
