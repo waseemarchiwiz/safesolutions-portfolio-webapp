d9bc08b24d94c76d6a6e6beafa5af905c2aaf5f4
wassii-khan <shreddedunion114@gmail.com>
2026-02-09 12:48:06 +0500

    fix: the route issue

--- src/components/nav-user.tsx
+++ src/components/nav-user.tsx
@@ -118,7 +118,7 @@ export function NavUser() {
             </DropdownMenuLabel>
 
             {/* Menu Items */}
-            <Link href="profile">
+            <Link href="/dashboard/profile">
               <DropdownMenuItem className="rounded-lg cursor-pointer group px-3 py-2.5">
                 <IconUser className="w-4 h-4 mr-3 text-slate-500 group-hover:text-slate-700" />
                 <span className="text-sm">Profile</span>
